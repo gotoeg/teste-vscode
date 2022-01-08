8c522c74da66fc6a4724e818dcf763a9188ae4b2
Goto <95108697+gotoeg@users.noreply.github.com>
2022-01-07 22:46:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 console.log ('Ola vivi, como vc está. Ficou presa em casa? Tudo certo, é só um teste. Mais um teste só.!!!!!&&&&&')
 console.log ('teste 2.!!!!%%%%%')
+console.log ('Olá mundo!!!!!!!!!!!!!!!!!!!!!!!!!!!$$$$$$$$$$$$$$$$$$$$$$$$$$')
